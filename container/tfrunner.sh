24d13919b714d218e4948aed3796a28f74eed09d
Roger Best <rbest@microsoft.com>
2020-03-04 15:34:43 -0800

    test logging

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -17,7 +17,7 @@ for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1
 trace "Connecting Azure ..."
 while true; do
     # managed identity isn't avaialble directly - retry
-    az login --identity -u $EnvironmentUserId  2>/dev/null && {
+    az login --identity 2>/dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
         export ARM_SUBSCRIPTION_ID=$(az account show --output=json | jq -r -M '.id')
@@ -29,6 +29,9 @@ done
 
 trace "Connecting AZ Copy ..."
 azcopy login --identity --identity-resource-id $EnvironmentUserId
+
+trace export
+trace "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$ARM_STORAGE_CONTAINER$STORAGE_PREFIX/*"
 
 trace "Copying files locally ..."
 azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$ARM_STORAGE_CONTAINER$STORAGE_PREFIX/*" "*" --recursive
