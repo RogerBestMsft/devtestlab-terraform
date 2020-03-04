55b085649bce223dc1f9116c89ea3a54c533b840
Roger Best <rbest@microsoft.com>
2020-03-04 14:31:16 -0800

    update tfrunner

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -8,18 +8,13 @@ trace "Setup folder structure ..."
 mkdir /runbooks 
 cd /runbooks
 
-#trace "Downloading runbooks ..."
-#for url in $*; do wget ${url}; done
-STORAGE_PREFIX=$1
-echo "TEST1: $STORAGE_PREFIX"
-
 trace "Cleanup runbooks ..."
 for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1); done
 
 trace "Connecting Azure ..."
 while true; do
     # managed identity isn't avaialble directly - retry
-    az login --identity 2>/dev/null && {
+    az login --identity -u $EnvironmentUserId  2>/dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
         export ARM_SUBSCRIPTION_ID=$(az account show --output=json | jq -r -M '.id')
@@ -30,14 +25,10 @@ while true; do
 done
 
 trace "Connecting AZ Copy ..."
-azcopy login --identity
+azcopy login --identity --identity-resource-id $EnvironmentUserId
 
 trace "Copying files locally ..."
-azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$ARM_STORAGE_CONTAINER/$STORAGE_PREFIX/*" "." --recursive
-
-#trace "Get storage files"
-#az storage blob download -s $ARM_STORAGE_CONTAINER -d ./local --pattern /subscriptions/da8f3095-ac12-4ef2-9b35-fcd24842e207/resourceGroups/testcustomrp-EchoEnv-669175/*.*
-#FileArray = az storage blob list --container-name $ARM_STORAGE_CONTAINER --prefix $STORAGE_PREFIX --query "[].{name:name}" -o tsv
+azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$ARM_STORAGE_CONTAINER$STORAGE_PREFIX/*" "." --recursive
 
 trace "Wait for Azure deployment ..."
 az group deployment wait --resource-group $EnvironmentResourceGroupName --name $EnvironmentDeploymentName --created
@@ -49,9 +40,10 @@ trace "Applying Terraform ..."
 terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
 
 if [ -z "$ContainerGroupId" ]; then
-    trace "Waiting for termination ..."
-    tail -f /dev/null
+    trace "Completed ..."
+    #tail -f /dev/null
 else
     trace "Deleting container groups ..."
     az container delete --yes --ids $ContainerGroupId
 fi
+touch /tmp/ready
