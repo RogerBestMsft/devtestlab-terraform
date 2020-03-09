5223654a30df8c0aa634f6a57b229f39bc7f4354
Roger Best <rbest@microsoft.com>
2020-03-09 13:32:29 -0700

    D worked E

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -7,25 +7,25 @@ trace() {
 
 export STORAGE_PREFIX=$1
 export APPLY_DEPLOYMENT=true
-#trace "Setup folder structure ..."
-#mkdir /runbooks 
-#cd /runbooks
+trace "Setup folder structure ..."
+mkdir /runbooks 
+cd ./runbooks
 
-#trace "Cleanup runbooks ..."
-#for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1); done
+trace "Cleanup runbooks ..."
+for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1); done
 
-#trace "Connecting Azure ..."
-#while true; do
+trace "Connecting Azure ..."
+while true; do
     # managed identity isn't avaialble directly - retry
-#    az login --identity -u $EnvironmentUserId 2>/dev/null && {
-#        export ARM_USE_MSI=true
-#        export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
-#        export ARM_SUBSCRIPTION_ID=$(az account show --output=json | jq -r -M '.id')
-#        export ARM_TENANT_ID=$(az account show --output=json | jq -r -M '.tenantId')
-#        export TF_LOG=trace
-#        break
-#    } || sleep 5    
-#done
+    az login --identity -u $EnvironmentUserId 2>/dev/null && {
+        export ARM_USE_MSI=true
+        export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
+        export ARM_SUBSCRIPTION_ID=$(az account show --output=json | jq -r -M '.id')
+        export ARM_TENANT_ID=$(az account show --output=json | jq -r -M '.tenantId')
+        export TF_LOG=trace
+        break
+    } || sleep 5    
+done
 
 #trace $EnvironmentUserId
 
@@ -35,7 +35,7 @@ azcopy login --identity --identity-resource-id $EnvironmentUserId
 #trace "$AZURE_STORAGE_ACCOUNT"
 #trace "$AZURE_STORAGE_CONTAINER"
 #trace "$STORAGE_PREFIX"
-#trace "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*"
+trace "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*"
 
 trace "Copying files locally ..."
 #azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*" "/runbooks" --recursive
