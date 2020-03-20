62919878941221b892bcc153440d52b104fdb70a
Roger Best <rbest@microsoft.com>
2020-03-20 11:43:03 -0700

    Update

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -6,7 +6,7 @@ trace() {
 }
 
 export STORAGE_PREFIX=$1
-export APPLY_DEPLOYMENT=true
+#export DEPLOYMENT_TYPE="create"
 trace "Setup folder structure ..."
 mkdir /runbooks 
 cd ./runbooks
@@ -30,10 +30,6 @@ done
 trace "Connecting AZ Copy ..."
 azcopy login --identity --identity-resource-id $EnvironmentUserId
 
-#trace "$AZURE_STORAGE_ACCOUNT"
-#trace "$AZURE_STORAGE_CONTAINER"
-#trace "$STORAGE_PREFIX"
-trace "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*"
 export SOURCE_URI="https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*"
 trace $SOURCE_URI
 trace "Copying files locally ..."
@@ -41,30 +37,30 @@ azcopy copy $SOURCE_URI "/runbooks" --recursive
 #azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*" "/runbooks" --recursive
 #azcopy copy "https://crpstoretcspbmuiw6fc2.blob.core.windows.net/environments-src-files/subscriptions/da8f3095-ac12-4ef2-9b35-fcd24842e207/resourceGroups/testcustomrp-BravoEnv-035234/*" "/runbooks" --recursive
 
-trace "Wait for Azure deployment ..."
-az group deployment wait --resource-group $EnvironmentResourceGroupName --name $EnvironmentDeploymentName --exists
+#trace "Wait for Azure deployment ..."
+#az group deployment wait --resource-group $EnvironmentResourceGroupName --name $EnvironmentDeploymentName --exists
 
 trace "Sleeping ..."
-sleep 30
+sleep 20
 
 trace "Initializing Terraform ..."
 terraform init -backend-config state.tf -reconfigure
 
 trace "Checking to apply or destroy ..."
-#if [$APPLY_DEPLOYMENT]; then
+if ["$DEPLOYMENT_TYPE" = "create"]; then
     trace "Applying Terraform ..."
     terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
-#else
-#    trace "Deleting Terraform ..."
-#    terraform destroy -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
-#fi
+else
+    trace "Deleting Terraform ..."
+    terraform destroy -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
+fi
 
 if [ -z "$ContainerGroupId" ]; then
     trace "Completed ..."
-    #tail -f /dev/null
 else
     trace "Deleting container groups ..."
     az container delete --yes --ids $ContainerGroupId
 fi
+
 trace "Set the readiness probe file ..."
 echo 'Completed' > ready.txt
