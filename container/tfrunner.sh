ed8338092eeb019c2f81369e4fcc9b1f0cfbf3d0
Roger Best <rbest@microsoft.com>
2020-03-24 11:28:55 -0700

    add delete

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -58,13 +58,14 @@ terraform init -backend-config state.tf -reconfigure
 #export DEPLOY_CREATE="create"
 trace "Before apply or destroy: $DEPLOYMENT_TYPE "
 trace "Checking to apply or destroy ..."
-#if [$DEPLOYMENT_TYPE == "create"]; then
+if [$DEPLOYMENT_TYPE == "delete"]; then
+    trace "Deleting Terraform ..."
+    terraform destroy -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
+else
     trace "Applying Terraform ..."
     terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
-#else
-#    trace "Deleting Terraform ..."
-#    terraform destroy -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
-#fi
+
+fi
 
 if [ -z "$ContainerGroupId" ]; then
     trace "Completed ..."
