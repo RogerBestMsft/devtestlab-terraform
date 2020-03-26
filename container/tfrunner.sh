7ab710abe2d47c3ba67f97f03734ad55c7c0c3ce
Roger Best <rbest@microsoft.com>
2020-03-26 10:15:21 -0700

    add sleep for debugging

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -35,6 +35,8 @@ export SOURCE_URI="https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_S
 trace "SourceURI: $SOURCE_URI"
 trace "Copying files locally ..."
 azcopy copy $SOURCE_URI "/runbooks" --recursive
+
+sleep 60
 #azcopy copy "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net$AZURE_STORAGE_CONTAINER$STORAGE_PREFIX/*" "/runbooks" --recursive
 #azcopy copy "https://crpstoretcspbmuiw6fc2.blob.core.windows.net/environments-src-files/subscriptions/da8f3095-ac12-4ef2-9b35-fcd24842e207/resourceGroups/testcustomrp-BravoEnv-035234/*" "/runbooks" --recursive
 
